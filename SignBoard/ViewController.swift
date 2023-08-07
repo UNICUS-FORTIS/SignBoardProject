9f31985eb0c0198af5e66e4d3003e7db4fd10dd3
Louie Shin <unicus.fortis@gmail.com>
2023-08-07 22:05:41 +0900

    incomplete

--- SignBoard/ViewController.swift
+++ SignBoard/ViewController.swift
@@ -26,7 +26,6 @@ class ViewController: UIViewController {
     
     let buttonBorderColor = UIColor.systemPink.cgColor
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
